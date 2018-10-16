0bb6e416d575149d8139bbdce8ba778db2a55e75
unknown <huutam.ptnk@gmail.com>
2018-10-16 11:33:12 +0300

    submited problem B successfully

--- CHEFPRMS/main.cpp
+++ CHEFPRMS/main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 
 using namespace std;
-int T, n, bang[300];
+int T, n, bang[300], a[200];
 int main()
 {
-    cin>>T;
     // 0 is prime, 1 is not
     bang[1]=1;
     bang[0]=1;
@@ -15,13 +14,25 @@ int main()
             }
         }
     }
-    for (int i=1; i<=100;i++){
-        if(bang[i]==0)
-            cout<<i<<" ";
+    for (int i=1; i<=200; i++){
+        for (int j=1; j<=200; j++){
+            if (bang[i]==0 && bang[j]==0 && i*j<=200 && i!=j){
+                a[i*j]=1;
+            }
+        }
     }
+    cin>>T;
     for (int t=0;t<T;t++){
         cin>>n;
-        for
+        int kt=0;
+        for (int i=1;i<=n-1;i++){
+            if (a[i]==1 && a[n-i]==1)
+                kt=1;
+        }
+        if (kt==0)
+            cout<<"NO"<<endl;
+        else
+            cout<<"YES"<<endl;
     }
     return 0;
 }
